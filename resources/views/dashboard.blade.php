e200cbbac6422a9e4a6ed9ba99a96650e8d42506
azamudio1 <azamudiokostina@safareyes.es>
2024-11-27 19:21:14 +0100

    update y correciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -29,12 +29,13 @@
         <p class="text-gray-600 mb-6">Elige un juego y empieza a divertirte.</p>
         
         <div class="space-y-4">
-            <a href="{{ route('adivinapalabra') }}" class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Adivina la Palabra</a>
-            <a href="{{ route('sopaletras') }}" class="block bg-green-500 hover:bg-green-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Sopa de Letras</a>
-            <a href="{{ route('tresEnRaya') }}" class="block bg-teal-500 hover:bg-teal-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Tres En Raya</a>
-            <a href="{{ route('relax') }}" class="block bg-teal-500 hover:bg-teal-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Relax</a>
-            <a href="{{ route('laberinto') }}" class="block bg-teal-500 hover:bg-teal-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Laberinto</a>
+            <a href="{{ route('adivinapalabra') }}" class="block bg-indigo-600 hover:bg-indigo-800 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Adivina la Palabra</a>
+            <a href="{{ route('sopaletras') }}" class="block bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Sopa de Letras</a>
+            <a href="{{ route('tresEnRaya') }}" class="block bg-red-500 hover:bg-red-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Tres En Raya</a>
+            <a href="{{ route('relax') }}" class="block bg-pink-500 hover:bg-pink-700 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Relax</a>
+            <a href="{{ route('laberinto') }}" class="block bg-teal-600 hover:bg-teal-800 text-white font-bold py-3 rounded-lg transition duration-300 ease-in-out">Laberinto</a>
         </div>
+
     </div>
 
     <script src="{{ asset('js/volumen.js') }}"></script>
